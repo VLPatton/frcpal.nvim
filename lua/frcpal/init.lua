33b80f107917038fd33143b8df8c2e687c416924
Harper Londyn <vlpatton@vlpatton.gay>
2023-07-18 10:31:45 -0500

    Finally add working (supposedly) support for term buf (init.lua)

--- lua/frcpal/init.lua
+++ lua/frcpal/init.lua
@@ -1,49 +1,19 @@
-local ui = require('frcpal.ui')
-local uv = vim.loop
+local uv, fn = vim.loop, vim.fn
 
 local M = {}
 
-
+-- Creates a new terminal buffer in the current window and opens gradlew. Note
+-- that $PWD must be the location of gradlew or it will fail.
 function M.gradle(cmd)
-    local gradlew = './gradlew'
-
-    local stdin = uv.new_pipe()
-    local stdout = uv.new_pipe()
-    local stderr = uv.new_pipe()
-
-    local handle, pid = uv.spawn(gradlew, {
-        args = {cmd},
-        stdio = {stdin, stdout, stderr}
-    }, function(code, signal)
-        print('exit code', code)
-    end)
-
-    assert(handle, 'process handle nil')
-    local winno = ui.create_window("Gradle")
-
-    uv.read_start(stdout, function(err, data)
-        assert(not err, err)
-        if data then
-            ui.output(data)
-        end
-    end)
-    
-    uv.read_start(stderr, function(err, data)
-        assert(not err, err)
-        if data then
-            ui.output(data)
-        end
-    end)
-
-    uv.shutdown(stdin, function()
-        print("stdin shutdown", stdin)
-        uv.close(handle, function()
-            print("process closed", handle, pid)
-        end)
-    end)
+    vim.cmd.enew()
+    local job_id = fn.termopen('./gradlew ' .. cmd, {
+        on_stderr = function()end,
+        on_stdout = function()end
+    })
 end
 
-
+-- Launches `curl` with the prompted URL and outputs the JSON file it receives
+-- in $PWD/vendordeps/.
 function M.get_vendordep()
     local curl = 'curl'
     local url = ''
@@ -51,6 +21,8 @@ function M.get_vendordep()
         url = input
     end)
 
+    -- We use UV here b/c it's how I originally designed this and this works
+    -- just fine so I'm not gonna break it :)
     local stdin = uv.new_pipe()
     local stdout = uv.new_pipe()
     local stderr = uv.new_pipe()
